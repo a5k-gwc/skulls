e16344951e396d5358304f2c1abfd4e203d5c119
ale5000 <ale5000-git@users.noreply.github.com>
2005-12-21 21:08:18 +0000

    Initial code commit

---
+++ Skulls/vars.php
@@ -0,0 +1,14 @@
+<?
+$ENABLED				= 1;
+
+$KICK_START_ENABLED		= 1;
+$LOG_ENABLED			= 1;
+
+$MAX_HOSTS				= 20;	// Maximum number of host stored for EACH network
+$MAX_HOSTS_OUT			= 20;	// Maximum number of host sent in each request
+
+$MAX_CACHES				= 35;	// Maximum number of cache stored for ALL networks
+$MAX_CACHES_OUT			= 20;	// Maximum number of cache sent in each request
+
+$TIMEOUT				= 20;	// Sockets time out
+?>
