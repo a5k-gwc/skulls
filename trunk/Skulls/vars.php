fc57491880cd4e5bfa89f763b8cf9aac45df718a
ale5000 <ale5000@088debd0-0f8b-4561-a1ad-651e8888c0ef>
2006-06-24 17:41:58 +0000

    *** empty log message ***

--- trunk/Skulls/vars.php
+++ trunk/Skulls/vars.php
@@ -1,9 +1,11 @@
 <?php
 $ENABLED				= 1;
 
-$KICK_START_ENABLED		= 1;
-$LOG_ENABLED			= 1;
 $STATFILE_ENABLED		= 1;
+$PING_WEBCACHES			= 1;	// Disable ONLY if the server have FSOCKOPEN disabled
+$KICK_START_ENABLED		= 1;	// KickStart should be DISABLED after populating the webcache
+
+$LOG_ENABLED			= 0;	// Enable ONLY for debugging
 
 $MAX_HOSTS				= 20;	// Maximum number of host stored for EACH network
 $MAX_HOSTS_OUT			= 20;	// Maximum number of host sent in each request
