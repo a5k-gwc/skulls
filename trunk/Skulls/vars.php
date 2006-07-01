629303f0f574557566b871bf5c1a61f7717a2b73
ale5000 <ale5000@088debd0-0f8b-4561-a1ad-651e8888c0ef>
2006-07-01 15:59:57 +0000

    *** empty log message ***

--- trunk/Skulls/vars.php
+++ trunk/Skulls/vars.php
@@ -7,10 +7,10 @@ $KICK_START_ENABLED		= 1;	// KickStart should be DISABLED after populating the w
 
 $LOG_ENABLED			= 0;	// Enable ONLY for debugging
 
-$MAX_HOSTS				= 20;	// Maximum number of host stored for EACH network (If there are 2 networks and this value is 20 -> 20 x 2 = 40)
+$MAX_HOSTS				= 30;	// Maximum number of host stored for EACH network (If there are 2 networks and this value is 30 -> 30 x 2 = 60)
 $MAX_HOSTS_OUT			= 20;	// Maximum number of host sent in each request
 
-$MAX_CACHES				= 35;	// Maximum number of cache stored for ALL networks
+$MAX_CACHES				= 40;	// Maximum number of cache stored for ALL networks
 $MAX_CACHES_OUT			= 20;	// Maximum number of cache sent in each request
 
 $TIMEOUT				= 20;	// Sockets time out
