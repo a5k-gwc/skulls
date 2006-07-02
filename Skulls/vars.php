804e8c0146d48b1c8e5b0fdd93203d41e16b2c0d
ale5000 <ale5000-git@users.noreply.github.com>
2006-07-02 17:00:14 +0000

    *** empty log message ***

--- Skulls/vars.php
+++ Skulls/vars.php
@@ -1,17 +1,17 @@
 <?php
-$ENABLED				= 1;
+$ENABLED =						1;
 
-$STATFILE_ENABLED		= 1;
-$PING_WEBCACHES			= 1;	// Disable ONLY if the server have FSOCKOPEN disabled
-$KICK_START_ENABLED		= 0;	// KickStart should be DISABLED after populating the webcache
+define( "PING_WEBCACHES",		1 );	// Disable ONLY if the server have FSOCKOPEN disabled
+define( "STATFILE_ENABLED",		1 );
+define( "KICK_START_ENABLED",	0 );	// KickStart should be DISABLED after populating the webcache
 
-$LOG_ENABLED			= 0;	// Enable ONLY for debugging
+define( "LOG_ENABLED",			0 );	// Enable ONLY for debugging
 
-$MAX_HOSTS				= 30;	// Maximum number of host stored for EACH network (If there are 2 networks and this value is 30 -> 30 x 2 = 60)
-$MAX_HOSTS_OUT			= 20;	// Maximum number of host sent in each request
+define( "MAX_HOSTS",			30 );	// Maximum number of host stored for EACH network (If there are 2 networks and this value is 30 -> 30 x 2 = 60)
+define( "MAX_HOSTS_OUT",		20 );	// Maximum number of host sent in each request
 
-$MAX_CACHES				= 40;	// Maximum number of cache stored for ALL networks
-$MAX_CACHES_OUT			= 20;	// Maximum number of cache sent in each request
+define( "MAX_CACHES",			40 );	// Maximum number of cache stored for ALL networks
+define( "MAX_CACHES_OUT",		20 );	// Maximum number of cache sent in each request
 
 $TIMEOUT				= 20;	// Sockets time out
 
