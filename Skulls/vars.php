9f808ff7a7b5cc6d9ed60c4824bb540555a46372
ale5000 <ale5000-git@users.noreply.github.com>
2006-07-20 23:19:11 +0000

    *** empty log message ***

--- Skulls/vars.php
+++ Skulls/vars.php
@@ -8,10 +8,10 @@ define( "KICK_START_ENABLED",	0 );	// KickStart should be DISABLED after populat
 define( "LOG_ENABLED",			0 );	// Enable logging of all requests (ONLY for debugging)
 define( "LOG_ERRORS",			0 );	// Enable logging of invalid requests (ONLY for debugging)
 
-define( "MAX_HOSTS",			30 );	// Maximum number of host stored for EACH network (If there are 2 networks and this value is 30 -> 30 x 2 = 60)
+define( "MAX_HOSTS",			25 );	// Maximum number of host stored for EACH network (If there are 2 networks and this value is 25 -> 25 x 2 = 50)
 define( "MAX_HOSTS_OUT",		20 );	// Maximum number of host sent in each request
 
-define( "MAX_CACHES",			60 );	// Maximum number of cache stored for ALL networks
+define( "MAX_CACHES",			50 );	// Maximum number of cache stored for ALL networks
 define( "MAX_CACHES_OUT",		15 );	// Maximum number of cache sent in each request
 
 $TIMEOUT =						20;		// Sockets time out
