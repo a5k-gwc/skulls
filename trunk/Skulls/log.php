6461ccfaa69d9928ab23c11c5d43db0f8504b16f
ale5000 <ale5000@088debd0-0f8b-4561-a1ad-651e8888c0ef>
2006-07-21 23:26:07 +0000

    *** empty log message ***

--- trunk/Skulls/log.php
+++ trunk/Skulls/log.php
@@ -8,11 +8,10 @@ function Logging($filename, $CLIENT, $VERSION, $NET)
 {
 	$HTTP_X_FORWARDED_FOR = isset($_SERVER["HTTP_X_FORWARDED_FOR"]) ? $_SERVER["HTTP_X_FORWARDED_FOR"] : "";
 	$HTTP_CLIENT_IP = isset($_SERVER["HTTP_CLIENT_IP"]) ? $_SERVER["HTTP_CLIENT_IP"] : "";
-	$HTTP_FROM = isset($_SERVER["HTTP_FROM"]) ? $_SERVER["HTTP_FROM"] : "";
 
 	$file = fopen("log/".$filename.".log", "a");
 	flock($file, 2);
-	fwrite($file, gmdate("Y/m/d h:i:s A")." | ".$CLIENT." ".$VERSION." | ".$_SERVER["HTTP_USER_AGENT"]." | ".$NET." | ".$_SERVER["QUERY_STRING"]." | ".$_SERVER["REMOTE_ADDR"]." | ".$HTTP_X_FORWARDED_FOR." | ".$HTTP_CLIENT_IP." | ".$HTTP_FROM."\r\n");
+	fwrite($file, gmdate("Y/m/d h:i:s A")." | ".$CLIENT." ".$VERSION." | ".$_SERVER["HTTP_USER_AGENT"]." | ".$NET." | ".$_SERVER["QUERY_STRING"]." | ".$_SERVER["REMOTE_ADDR"]." | ".$HTTP_X_FORWARDED_FOR." | ".$HTTP_CLIENT_IP."\r\n");
 	flock($file, 3);
 	fclose($file);
 }
