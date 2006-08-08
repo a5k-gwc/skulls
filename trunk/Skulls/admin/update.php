2625a390aeb8af5238a08d560debb6b24954f497
ale5000 <ale5000@088debd0-0f8b-4561-a1ad-651e8888c0ef>
2006-08-08 18:49:52 +0000

    *** empty log message ***

--- trunk/Skulls/admin/update.php
+++ trunk/Skulls/admin/update.php
@@ -1,7 +1,7 @@
 <?php
 header("Pragma: no-cache");
 
-define( "REVISION", 4 );
+define( "REVISION", 4.1 );
 if( !file_exists("revision.dat") )
 {
 	$file = fopen("revision.dat", "x");
@@ -11,8 +11,11 @@ if( !file_exists("revision.dat") )
 	fclose($file);
 }
 $file_content = file("revision.dat");
-if($file_content[0] >= REVISION)
-	die("There is no need to update it.<br/>\r\nThis file checks only if data files are updated, it doesn't check if Skulls is updated.<br/>\r\n");
+if(trim($file_content[0]) >= REVISION)
+{
+	header("Content-Type: text/plain");
+	die("There is no need to update it.\r\nThis file checks only if data files are updated, it doesn't check if Skulls is updated.\r\n");
+}
 
 
 $log = "";
@@ -76,13 +79,13 @@ if( file_exists("../".DATA_DIR."/hosts_gnutella1.dat") )
 	if( !file_exists("../".DATA_DIR."/hosts_gnutella.dat") )
 	{
 		$result = rename("../".DATA_DIR."/hosts_gnutella1.dat", "../".DATA_DIR."/hosts_gnutella.dat");
-		$log .= "Renaming hosts_gnutella1.dat to hosts_gnutella.dat: ";
+		$log .= "Renaming ".DATA_DIR."/hosts_gnutella1.dat to ".DATA_DIR."/hosts_gnutella.dat: ";
 		$log .= check($result);
 	}
 	else
 	{
 		$result = unlink("../".DATA_DIR."/hosts_gnutella1.dat");
-		$log .= "Deleting hosts_gnutella1.dat: ";
+		$log .= "Deleting ".DATA_DIR."/hosts_gnutella1.dat: ";
 		$log .= check($result);
 	}
 }
@@ -113,56 +116,16 @@ if( file_exists("../".DATA_DIR."/caches.dat") )
 
 	if($changed)
 	{
-		$log .= "Internal structure updated in caches.dat.<br/>\r\n";
+		$log .= "Internal structure updated in ".DATA_DIR."/caches.dat.<br/>\r\n";
 		$updated = TRUE;
 	}
 }
 
 if( file_exists("../".DATA_DIR."/blocked_caches.dat") )
 {
-	$data = file("../".DATA_DIR."/blocked_caches.dat");
-	$count_blocked_caches = count($data);
-
-	$new_blocked_caches[] = array("http://www.exactmobile.co.za/cache.asp", 0);
-	$new_blocked_caches[] = array("http://www.exactmobile.co.za/cache.asp/", 0);
-	$new_blocked_caches[] = array("http://www.sexymobile.co.za/cache.asp", 0);
-	$count_new_blocked_caches = count($new_blocked_caches);
-
-	$changed = FALSE;
-	for($i = 0; $i < $count_blocked_caches; $i++)
-	{
-		$data[$i] = trim($data[$i]);
-		for($x = 0; $x < $count_new_blocked_caches; $x++)
-		{
-			if($data[$i] == $new_blocked_caches[$x][0])
-				$new_blocked_caches[$x][1] = 1;
-		}
-	}
-
-	for($x = 0; $x < $count_new_blocked_caches; $x++)
-		if($new_blocked_caches[$x][1] == 0)
-			$changed = TRUE;
-
-	$file = fopen("../".DATA_DIR."/blocked_caches.dat", "w");
-	flock($file, 2);
-	for($i = 0; $i < $count_blocked_caches; $i++)
-	{
-		if( $data[$i] != "http://gwc.wodi.org/g2/bazooka" )	// Old blocked cache
-			fwrite($file, $data[$i]."\r\n");
-		else
-			$changed = TRUE;
-	}
-	for($x = 0; $x < $count_new_blocked_caches; $x++)
-		if($new_blocked_caches[$x][1] == 0)
-			fwrite($file, $new_blocked_caches[$x][0]."\r\n");
-	flock($file, 3);
-	fclose($file);
-
-	if($changed)
-	{
-		$log .= "blocked_caches.dat updated.<br/>\r\n";
-		$updated = TRUE;
-	}
+	$result = unlink("../".DATA_DIR."/blocked_caches.dat");
+	$log .= "Deleting ".DATA_DIR."/blocked_caches.dat: ";
+	$log .= check($result);
 }
 
 if( !file_exists("../".DATA_DIR."/failed_urls.dat") )
@@ -184,7 +147,7 @@ if( !file_exists("../".DATA_DIR."/failed_urls.dat") )
 if( file_exists("../log/skulls.log") )
 {
 	$result = unlink("../log/skulls.log");
-	$log .= "Deleting skulls.log: ";
+	$log .= "Deleting log/skulls.log: ";
 	$log .= check($result);
 }
 
