0221a1ec7e422dea45c16dc1113bc69182dacf8d
ale5000 <ale5000-git@users.noreply.github.com>
2006-12-10 02:54:13 +0000

    *** empty log message ***

--- Skulls/log.php
+++ Skulls/log.php
@@ -6,12 +6,13 @@ if( !file_exists("log/") )
 
 function Logging($filename, $CLIENT, $VERSION, $NET)
 {
+	global $USER_AGENT;
 	$HTTP_X_FORWARDED_FOR = isset($_SERVER["HTTP_X_FORWARDED_FOR"]) ? $_SERVER["HTTP_X_FORWARDED_FOR"] : "";
 	$HTTP_CLIENT_IP = isset($_SERVER["HTTP_CLIENT_IP"]) ? $_SERVER["HTTP_CLIENT_IP"] : "";
 
 	$file = fopen("log/".$filename.".log", "a");
 	flock($file, 2);
-	fwrite($file, gmdate("Y/m/d H:i:s")." | ".$CLIENT." ".$VERSION." | ".$_SERVER["HTTP_USER_AGENT"]." | ".$NET." | ".$_SERVER["QUERY_STRING"]." | ".$_SERVER["REMOTE_ADDR"]." | ".$HTTP_X_FORWARDED_FOR." | ".$HTTP_CLIENT_IP."\r\n");
+	fwrite($file, gmdate("Y/m/d H:i:s")." | ".$CLIENT." ".$VERSION." | ".$USER_AGENT." | ".$NET." | ".$_SERVER["QUERY_STRING"]." | ".$_SERVER["REMOTE_ADDR"]." | ".$HTTP_X_FORWARDED_FOR." | ".$HTTP_CLIENT_IP."\r\n");
 	flock($file, 3);
 	fclose($file);
 }
--- Skulls/web_interface.php
+++ Skulls/web_interface.php
@@ -1,13 +1,175 @@
 <?php
-function ShowHtmlPage($num){
-	global $NET;
-	if( !function_exists("ReplaceVendorCode") )
+function ReplaceVendorCode($client, $version){
+	$cache = 0;
+	if( $client == "TEST" && (float)$version == 0 && substr($version, 0, 1) != "0" )
 	{
-		include "functions.php";
+		$client = $version;
+		$version = "";
+		$cache = 1;
 	}
 
-	if($NET == NULL)
-		$NET = "all";
+	switch($client)
+	{
+		case "ACQL":
+			$client_name = "Acqlite";
+			$url = "http://acqlite.sourceforge.net/";
+			break;
+		case "ACQX":
+			$client_name = "Acquisition";
+			$url = "http://www.acquisitionx.com/";
+			break;
+		case "AGIO":
+			$client_name = "Adagio";
+			$url = "http://sourceforge.net/projects/agio/";
+			break;
+		case "BEAR":
+			$client_name = "BearShare";
+			$url = "http://www.bearshare.com/";
+			break;
+		case "COCO":
+			$client_name = "CocoGnut";
+			$url = "http://www.alpha-programming.co.uk/software/cocognut/";
+			break;
+		case "DNET":
+			$client_name = "Deepnet Explorer";
+			$url = "http://www.deepnetexplorer.com/";
+			break;
+		case "GDNA":
+			$client_name = "GnucDNA";
+			$url = "http://www.gnucleus.com/GnucDNA/";
+			break;
+		case "GIFT":
+			$client_name = "giFT";
+			$url = "http://gift.sourceforge.net/";
+			break;
+		case "GNUC":
+			$client_name = "Gnucleus";
+			$url = "http://www.gnucleus.com/Gnucleus/";
+			break;
+		case "GNZL":
+			$client_name = "Gnoozle";
+			$url = "";
+			break;
+		case "GOLD":
+			$client_name = "Ares Gold";
+			$url = "";
+			break;
+		case "GPUX":
+			$client_name = "GPU";
+			$url = "http://sourceforge.net/projects/gpu/";
+			break;
+		case "GTKG":
+			$client_name = "GTK Gnutella";
+			$url = "http://gtk-gnutella.sourceforge.net/";
+			break;
+		case "LIME":
+			$client_name = "LimeWire";
+			$url = "http://www.limewire.com/";
+			break;
+		case "MESH":
+			$client_name = "iMesh";
+			$url = "http://www.imesh.com/";
+			break;
+		case "MLDK":
+			$client_name = "MLDonkey";
+			$url = "http://www.mldonkey.net/";
+			break;
+		case "MMMM":
+		case "MRPH":
+			$client_name = "Morpheus";
+			$url = "http://www.morpheus.com/";
+			break;
+		case "MNAP":
+			$client_name = "MyNapster";
+			$url = "http://www.mynapster.com/";
+			break;
+		case "MUTE":
+			$client_name = "Mutella";
+			$url = "http://mutella.sourceforge.net/";
+			break;
+		case "MXIE":
+			$client_name = "mxie";
+			$url = "http://www.mxie.com/";
+			break;
+		case "NOVA":
+			$client_name = "Nova";
+			$url = "http://novap2p.sourceforge.net/";
+			break;
+		case "PHEX":
+			$client_name = "Phex";
+			$url = "http://phex.kouk.de/mambo/";
+			break;
+		case "RAZA":
+			$client_name = "Shareaza";
+			$url = "http://www.shareaza.com/";
+			break;
+		case "RAZB":
+			$client_name = "SBeta (Shareaza beta)";		// Beta version of Shareaza
+			$url = "http://www.shareaza.com/beta/";
+			break;
+		case "SNOW":
+			$client_name = "FrostWire";
+			$url = "http://www.frostwire.com/";
+			break;
+		case "SWAP":
+			$client_name = "Swapper";
+			$url = "http://www.revolutionarystuff.com/swapper/";
+			break;
+		case "TFLS":
+			$client_name = "TrustyFiles";
+			$url = "http://www.trustyfiles.com/";
+			break;
+		case "XOLO":
+			$client_name = "XoloX";
+			$url = "http://www.xolox.nl/";
+			break;
+
+		case "PGDBScan":
+			$client_name = "Jonatkins scan";
+			$url = "http://gcachescan.jonatkins.com/";
+			break;
+		case "KICKSTART":
+			$client_name = "KickStart";
+			$url = "";
+			break;
+		case "TEST":
+			$client_name = "WebCache";
+			$url = "";
+			break;
+
+		case "BAZK":
+			$client_name = "Bazooka (WebCache)";
+			$url = "http://rocketx.port5.com/";
+			break;
+		case "GCII":
+			$client_name = "PHPGnuCacheII (WebCache)";
+			$url = "http://gwcii.sourceforge.net/";
+			break;
+		case "SKLL":
+			$client_name = "Skulls (WebCache)";
+			$url = "http://ale5000.altervista.org/software.htm";
+			break;
+
+		default:
+			if($cache)
+				$client_name = "WebCache (".$client.")";
+			elseif( $client != "" )
+				$client_name = $client;
+			else
+				$client_name = "Unknown client";
+
+			$url = "";
+	}
+
+	if( $url != "" )
+		return "<a href=\"".$url."\" target=\"_blank\">".$client_name." ".$version."</a>";
+	else
+		return $client_name." ".$version;
+}
+
+function ShowHtmlPage($num){
+	global $NET;
+	if($NET == NULL) $NET = "all";
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
@@ -139,15 +301,21 @@ function ShowHtmlPage($num){
 												for( $i = $elements - 1; $i >= 0; $i-- )
 												{
 													list ($ip, $leaves, , $client, $version, $time) = explode("|", $host_file["host"][$i]);
+													if(isset($host_file["net"][$i])) $net = $host_file["net"][$i];
 													$color = $i % 2 == 0 ? "#F0F0F0" : "#FFFFFF";
 
 													echo "<tr align=\"left\" bgcolor=\"".$color."\">";
-													echo "<td style=\"padding-right: 10pt;\"><a href=\"gnutella:host:".$ip."\">".$ip."</a>";
+													echo "<td style=\"padding-right: 10pt;\">";
+													if(strpos(strtolower($net), "gnutella") > -1)
+														$url = "gnutella:host:";
+													else
+														$url = "http://";
+													echo "<a href=\"".$url.$ip."\">".$ip."</a>";
 													if( !empty($leaves) )
 														echo " (".$leaves.")";
 													echo "</td>";
 													echo "<td style=\"padding-right: 20pt;\"><strong>".ReplaceVendorCode($client, $version)."</strong></td>";
-													echo "<td style=\"padding-right: 20pt;\">".(isset($net) ? $net : $host_file["net"][$i])."</td>";
+													echo "<td style=\"padding-right: 20pt;\">".$net."</td>";
 													echo "<td>".$time."</td></tr>";
 												}
 											}
@@ -204,15 +372,20 @@ function ShowHtmlPage($num){
 													echo "<td style=\"padding-right: 10pt;\">";
 													echo "<a href=\"".$cache_url."\" target=\"_blank\">";
 
-													list( , $cache_url ) = explode("://", $cache_url, 2);
+													list($protocol, $cache_url) = explode("://", $cache_url, 2);
+													$max_length = 35;
 
-													if( strlen($cache_url) > 27 )
+													if( strlen($cache_url) > $max_length )
 													{
-														echo strpos($cache_url, "/") > 0 ? substr( $cache_url, 0, strpos($cache_url, "/") ) : substr( $cache_url, 0, 22 )." ... ";
-														echo "/";
+														$pos = strpos($cache_url, "/");
+														echo $protocol."://";
+														if($pos > 0 && $pos <= $max_length)
+															echo substr($cache_url, 0, $pos)."/...";
+														else
+															echo substr($cache_url, 0, $max_length)."...";
 													}
 													else
-														echo $cache_url;
+														echo $protocol."://".$cache_url;
 
 													echo "</a></td>";
 													echo "<td style=\"padding-right: 20pt;\">".$cache_name."</td>";
