1dea09a47081197d4395990024f058b13d1ac88f
ale5000 <ale5000-git@users.noreply.github.com>
2015-03-19 22:42:13 +0000

    Changed license to GPLv3

--- skulls/web_functions.php
+++ skulls/web_functions.php
@@ -1,4 +1,22 @@
 <?php
+//
+//  Copyright (C) 2005-2008, 2015 by ale5000
+//  This file is part of Skulls! Multi-Network WebCache.
+//
+//  Skulls is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Skulls is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Skulls.  If not, see <http://www.gnu.org/licenses/>.
+//
+
 function ReplaceVendorCode($client, $version){
 	$cache = 0;
 	if( $client == "TEST" && (float)$version == 0 && substr($version, 0, 1) != "0" )	// If $client is TEST and $version isn't numeric...
