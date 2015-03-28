00d372b22e904720cb8c67505da8b890c6f478ac
ale5000 <ale5000@088debd0-0f8b-4561-a1ad-651e8888c0ef>
2015-03-28 02:53:19 +0000

    Minor change

--- trunk/skulls/skulls.php
+++ trunk/skulls/skulls.php
@@ -79,7 +79,7 @@ function NormalizeIdentity(&$vendor, &$ver, $user_agent)
 	/* Change vendor code of mod versions of Shareaza */
 	if($vendor === 'RAZA')
 	{
-		if(strpos($user_agent, 'Shareaza') !== 0 || strpos($user_agent, 'Shareaza PRO') === 0)
+		if(strpos($user_agent, 'Shareaza ') !== 0 || strpos($user_agent, 'Shareaza PRO') === 0)
 			$vendor = 'RAZM';
 	}
 	elseif($vendor === 'LIME')
