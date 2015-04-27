ab4831b1a9363cb51e06ec8a6ced7d5c4d229737
ale5000 <ale5000@088debd0-0f8b-4561-a1ad-651e8888c0ef>
2015-04-27 17:35:01 +0000

    Switched doctype to HTML 4.01 Strict

--- trunk/skulls/web_interface.php
+++ trunk/skulls/web_interface.php
@@ -27,7 +27,7 @@ function ShowHtmlPage($num, $header, $footer)
 
 	Initialize($SUPPORTED_NETWORKS, TRUE, TRUE);
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
