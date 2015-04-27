9655e9c8a7425fd6eddabc7a979f98792324268e
ale5000 <ale5000@088debd0-0f8b-4561-a1ad-651e8888c0ef>
2015-04-27 19:49:20 +0000

    Declared English language

--- trunk/skulls/web_interface.php
+++ trunk/skulls/web_interface.php
@@ -28,7 +28,7 @@ function ShowHtmlPage($num, $header, $footer)
 	Initialize($SUPPORTED_NETWORKS, TRUE, TRUE);
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html>
+<html lang="en">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
