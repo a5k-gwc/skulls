8e83fe0043c1ad2439771526f9df51849be9f958
ale5000 <ale5000@088debd0-0f8b-4561-a1ad-651e8888c0ef>
2015-05-29 20:24:30 +0000

    Increased number of hosts/urls returned

--- trunk/skulls/vars.php
+++ trunk/skulls/vars.php
@@ -24,10 +24,10 @@ define( 'LOG_MAJOR_ERRORS',			1 );	// Enable logging of major errors
 define( 'LOG_MINOR_ERRORS',			0 );	// Enable logging of minor errors (NOTE: This is only for debugging, usually it should be kept disabled since it increase server load)
 
 define( 'MAX_HOSTS',				100 );	// Maximum number of host stored for EACH network (If there are 2 networks and this value is 30 then 30 x 2 = 60 - Setting this value too high DECREASE SPEED of the webcache)
-define( 'MAX_HOSTS_OUT',			40 );	// Maximum number of host sent in each request (By setting this value too high you can WASTE BANDWIDTH, by setting too low you can increase the number of requests)
+define( 'MAX_HOSTS_OUT',			50 );	// Maximum number of host sent in each request (By setting this value too high you can WASTE BANDWIDTH, by setting too low you can increase the number of requests)
 
 define( 'MAX_CACHES',				100 );	// Maximum number of cache stored for ALL networks (Setting this value too high DECREASE SPEED of the webcache)
-define( 'MAX_CACHES_OUT',			10 );	// Maximum number of cache sent in each request (By setting this value too high you can WASTE BANDWIDTH, by setting too low you can increase the number of requests)
+define( 'MAX_CACHES_OUT',			15 );	// Maximum number of cache sent in each request (By setting this value too high you can WASTE BANDWIDTH, by setting too low you can increase the number of requests)
 define( 'MAX_UDP_CACHES_OUT',		5 );
 
 define( 'RECHECK_CACHES',			24 );	// Hours to recheck a good cache
