507afda674975fc247dbcf64bfebb24993a0d245
ale5000 <ale5000-git@users.noreply.github.com>
2015-10-21 15:51:12 +0000

    Blocked a died GWC

--- skulls/skulls.php
+++ skulls/skulls.php
@@ -481,6 +481,7 @@ function CheckBlockedGWC($gwc_url)
 		$gwc_url === 'http://cache.trillinux.org/g2/bazooka.php'	/* Bugged - Return hosts with negative age */
 		|| $gwc_url === 'http://fascination77.free.fr/cachechu/'	/* Bugged - Call to undefined function: stream_socket_client() */
 		|| $gwc_url === 'http://peerproject.org/webcache/'			/* Duplicate URL */
+		|| $gwc_url === 'http://gwc.gofoxy.net:2108/gwc/cgi-bin/fc'	/* No longer exist */
 		|| $gwc_url === 'http://gwc.iblinx.com:2108/gwc/cgi-bin/fc'	/* No longer exist */
 		|| $gwc_url === 'http://gamagic.com:2108/gwc/cgi-bin/fc'	/* No longer exist */
 		|| $gwc_url === 'http://0517play.com:4400/gwc/cgi-bin/fc'	/* No longer exist */
