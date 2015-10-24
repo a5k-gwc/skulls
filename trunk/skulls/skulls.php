a8fd0d1c8ffe50e9218ac2d22c4ed842cde90509
ale5000 <ale5000@088debd0-0f8b-4561-a1ad-651e8888c0ef>
2015-10-24 05:47:00 +0000

    Bump version

--- trunk/skulls/skulls.php
+++ trunk/skulls/skulls.php
@@ -22,8 +22,8 @@ include 'vars.php';
 
 define('NAME', 'Skulls');
 define('VENDOR', 'SKLL');										/* Vendor code (four uppercase letters) */
-define('SHORT_VER', '0.3.1');									/* Numeric version (without letters) */
-define('VER', SHORT_VER.'c');									/* Full version (it can contain letters) */
+define('SHORT_VER', '0.3.2');									/* Numeric version (without letters) */
+define('VER', SHORT_VER.'');									/* Full version (it can contain letters) */
 define('GWC_SITE', 'http://sourceforge.net/projects/skulls/');	/* Official site of this GWC */
 define('OPEN_SOURCE', '1');
 define('MAX_HOST_AGE', 259200);									/* 3 days */
