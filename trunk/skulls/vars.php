9c81b61cf18642d30557ff41a5ed3f4fd14700f6
ale5000 <ale5000@088debd0-0f8b-4561-a1ad-651e8888c0ef>
2016-02-11 02:33:36 +0000

    Reduced timeout for reading data remotely.

--- trunk/skulls/vars.php
+++ trunk/skulls/vars.php
@@ -33,7 +33,7 @@ define( 'MAX_UDP_CACHES_OUT',		5 );
 
 define( 'RECHECK_CACHES',			24 );	// Hours to recheck a good cache
 define( 'CONNECT_TIMEOUT',			10 );	// Socket connection timeout
-define( 'TIMEOUT',					15 );	// Socket timeout
+define( 'TIMEOUT',					8 );	// Socket timeout
 
 define('VERIFY_HOSTS', true);
 define('ENABLE_URL_SUBMIT', true);  // Enable the submission of alternative GWCs, if it is disabled in addition of rejecting url submissions it will not return any previous url in queries although they will be still visible in the web interface
