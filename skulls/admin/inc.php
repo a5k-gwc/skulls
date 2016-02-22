9e0fdfdf3825a89e67864cf0446adaf49ef06d07
ale5000 <ale5000-git@users.noreply.github.com>
2016-02-22 04:34:51 +0000

    Further improved the data updater. Bump revision of the data updater.

--- skulls/admin/inc.php
+++ skulls/admin/inc.php
@@ -1,4 +1,7 @@
 <?php
-header("Content-Type: text/plain");
+header($_SERVER['SERVER_PROTOCOL'].' 200 OK'); list(,$prot_ver) = explode('/', $_SERVER['SERVER_PROTOCOL'], 2);
+header('Content-Type: text/plain');
+if($prot_ver >= 1.1) header('Cache-Control: no-cache'); else header('Pragma: no-cache');
+
 echo "\r\n";
 ?>
