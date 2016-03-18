05fe31ca3b02da1b3396681d30d8c7374b4f7ee5
ale5000 <ale5000-git@users.noreply.github.com>
2016-03-18 04:01:49 +0000

    Enable the update workaround also for MUTE MFC.

--- skulls/skulls.php
+++ skulls/skulls.php
@@ -1805,7 +1805,7 @@ else
 	if($SUPPORT)
 		Support($SUPPORT, $SUPPORTED_NETWORKS);
 
-	if($HOST !== null && ($CLIENT === 'MUTE' || $CLIENT === 'ANTS' || $CLIENT === 'GPUX'))
+	if($HOST !== null && ($CLIENT === 'MUTE' || $CLIENT === 'MMFC' || $CLIENT === 'ANTS' || $CLIENT === 'GPUX'))
 		if(IsValidPrivateIP($IP)) { $IP = $REMOTE_IP; $HOST = $IP.':'.$PORT; }  /* Workaround for a bug in own IP detection of some clients */
 
 	$is_good_update = null;
