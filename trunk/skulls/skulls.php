1e2478652132de7c577257284d93b63c52a8c7a8
ale5000 <ale5000@088debd0-0f8b-4561-a1ad-651e8888c0ef>
2016-04-28 01:59:55 +0000

    Use the blocklist also for GWC urls.

--- trunk/skulls/skulls.php
+++ trunk/skulls/skulls.php
@@ -1105,7 +1105,15 @@ function WriteCacheFile($file_path, $is_udp, $gwc_url, $client, $version, $is_a_
 			/* If the DNS resolution fails we assume it is already an IP address */
 			$gwc_ip = ResolveDNS($temp); if($gwc_ip === false) { $gwc_ip = $temp; $new_specs_only = '1'; } elseif(strpos($gwc_url, 'https://') === 0) $new_specs_only = '1';
 			if(DEBUG) echo 'D|update|GWC|IP|'.RemoveGarbage($gwc_ip)."\r\n";
-			if(!ValidateIP($gwc_ip))  return 4;  // Blocked URL
+			if(!ValidateIP($gwc_ip)) return 4;  // Blocked URL
+
+			include_once './update.php';
+			if(IsIPInBlockList($gwc_ip))
+			{
+				AddFailedUrl($gwc_url);
+				ReplaceCache($file_path, $i, $cache_file, null);
+				return 4;  // Blocked URL
+			}
 
 			$cache_data = CheckGWC(($is_udp? 'uhc:' : "").$gwc_url, $net_param, true);
 
@@ -1138,7 +1146,14 @@ function WriteCacheFile($file_path, $is_udp, $gwc_url, $client, $version, $is_a_
 		/* If the DNS resolution fails we assume it is already an IP address */
 		$gwc_ip = ResolveDNS($temp); if($gwc_ip === false) { $gwc_ip = $temp; $new_specs_only = '1'; } elseif(strpos($gwc_url, 'https://') === 0) $new_specs_only = '1';
 		if(DEBUG) echo 'D|update|GWC|IP|'.RemoveGarbage($gwc_ip)."\r\n";
-		if(!ValidateIP($gwc_ip))  return 4;  // Blocked URL
+		if(!ValidateIP($gwc_ip)) return 4;  // Blocked URL
+
+		include_once './update.php';
+		if(IsIPInBlockList($gwc_ip))
+		{
+			AddFailedUrl($gwc_url);
+			return 4;  // Blocked URL
+		}
 
 		$cache_data = CheckGWC(($is_udp? 'uhc:' : "").$gwc_url);
 
