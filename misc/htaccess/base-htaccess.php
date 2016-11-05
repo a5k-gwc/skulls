79f85c35267a7b64687a749a565e7fbd90f14810
ale5000 <ale5000-git@users.noreply.github.com>
2016-11-05 17:53:51 +0000

    Tagged 0.3.2.e (2016-04-14)

--- misc/htaccess/base-htaccess.php
+++
@@ -1,9 +0,0 @@
-<?php die(); ?>
-
-## Block bad clients ###
-<Files "skulls.php">
-  # Block Shareaza without version (it abuse server resources, it is blocked also inside the php script)
-  BrowserMatch "^Shareaza$" GoAway=1
-  Order Deny,Allow
-  Deny from Env=GoAway
-</Files>
