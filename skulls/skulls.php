db894b7a00d142b3b00e26d29c661f98bb9495f8
ale5000 <ale5000-git@users.noreply.github.com>
2019-12-22 21:07:50 +0100

    Shorten copyright text
    Update copyright year

--- skulls/skulls.php
+++ skulls/skulls.php
@@ -1,21 +1,11 @@
 <?php
-//  skulls.php
-//
-//  Copyright © 2005-2008, 2015-2016  ale5000
-//  This file is part of Skulls! Multi-Network WebCache.
-//
-//  Skulls is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  Skulls is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with Skulls.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ * skulls.php
+ * This file is part of Skulls! Multi-Network WebCache
+ *
+ * @copyright © 2005-2008, 2015-2016, 2019 ale5000
+ * SPDX-License-Identifer: GPL-3.0-or-later
+ */
 
 $SUPPORTED_NETWORKS = array();
 include './vars.php';
